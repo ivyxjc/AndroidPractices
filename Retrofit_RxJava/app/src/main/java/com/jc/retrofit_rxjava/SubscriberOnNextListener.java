328606e97b034a9abd671d4fe2cb9353a97353d8
ivyxjc <ivyxjc1994@hotmail.com>
2016-11-26 13:15:51 +0800

    autoUpdate

--- Retrofit_RxJava/app/src/main/java/com/jc/retrofit_rxjava/SubscriberOnNextListener.java
+++
@@ -1,9 +0,0 @@
-package com.jc.retrofit_rxjava;
-
-/**
- * Created by jc on 11/24/2016.
- */
-
-public interface SubscriberOnNextListener<T> {
-    void onNext(T t);
-}
--- Retrofit_RxJava/app/src/main/java/com/jc/retrofit_rxjava/model/MovieEntity.java
+++
@@ -1,8 +0,0 @@
-package com.jc.retrofit_rxjava.model;
-
-/**
- * Created by jc on 11/22/2016.
- */
-
-public class MovieEntity {
-}
