041062351dbd35d49989da5511663d375047c0f5
ivyxjc <ivyxjc1994@hotmail.com>
2016-12-06 21:50:34 +0800

    git ignore test

--- UltraPullToRefreshDemo/app/src/main/java/com/jc/ultrapulltorefreshdemo/net/HttpMethod.java
+++ UltraPullToRefreshDemo/app/src/main/java/com/jc/ultrapulltorefreshdemo/net/HttpMethod.java
@@ -1,5 +1,8 @@
 package com.jc.ultrapulltorefreshdemo.net;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -15,11 +18,26 @@ public class HttpMethod {
 
     private static class RetrofitStaticHolder{
         private static final Retrofit sRetrofit=new Retrofit.Builder()
+                .client(getOkHttp())
                 .baseUrl(Api.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .a
                 .build();
+    }
 
+    private static class OkHttpStaticHolder{
+        private static final OkHttpClient sOkHttp=new OkHttpClient.Builder()
+                                .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+                                .build();
+    }
+
+
+    public static OkHttpClient getOkHttp(){
+        return OkHttpStaticHolder.sOkHttp;
+    }
+
+    //单例
+    public static Retrofit getRetrofit(){
+        return RetrofitStaticHolder.sRetrofit;
     }
 }
